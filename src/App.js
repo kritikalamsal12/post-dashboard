a48452aeee9a72a2aeeba0aa6c4daf2c0fc8ba24
kritikalamsal12 <kritika.lamsal.98@gmail.com>
2022-04-25 00:18:26 +0530

    dbadded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
+import { Button, FormControl, TextField } from "@mui/material";
+import React, { useState, useEffect } from "react";
 import './App.css';
+import Accordion from './Accordion';
+import axios from "axios";
+import Loader from './Loader';
 
 function App() {
+  const getDataUrl = "https://my-json-server.typicode.com/kritikalamsal12/post-dashboard/db";
+
+  const [post, setPost] = useState({
+    userName: '',
+    title: '',
+    body: ''
+  });
+  const [posts, setPosts] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const [index, setIndex] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get(getDataUrl).then((response) => {
+      setPosts(response.data.posts);
+      setLoading(false);
+    }).catch(error => {
+      console.log(error);
+    });
+  }, []);
+
+  const createPost = (event) => {
+    const { name = '', value = '' } = event.target;
+    setPost({ ...post, [name]: value });
+  }
+
+  const editPostSubmit = () => {
+    const newPosts = [...posts];
+    newPosts.splice(index, 1, post);
+    setPost({
+      userName: '',
+      title: '',
+      body: ''
+    });
+    setPosts(newPosts);
+    setEdit(false);
+  }
+
+  const editPost = (index, post) => {
+    setPost(post);
+    document.getElementById('name').focus();
+    setEdit(true);
+    setIndex(index);
+  };
+
+  const deletePost = index => {
+    if(edit){
+      setEdit(false);
+      return setPost({
+        userName: '',
+        title: '',
+        body: ''
+      });
+    }
+    const newPosts = [...posts];
+    newPosts.splice(index, 1);
+    setPosts(newPosts);
+  };
+  const submit = () => {
+    if(edit){
+      return editPostSubmit();
+    }
+    if(post.userName.length!==0 && post.title.length!==0 && post.body.length!==0) {
+      setPosts([...posts, post]);
+    setPost({
+      userName: '',
+      title: '',
+      body: ''
+    });
+  }
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormControl>
+        <TextField
+          id="name"
+          label="Name"
+          name="userName"
+          value={post.userName}
+          onChange={createPost}
+          required
+        />
+
+        <TextField
+          label="Title"
+          name="title" value={post.title}
+          onChange={createPost}
+          style={{ margin: '20px 0px' }}
+          required
+        />
+
+        <TextField
+          label="Comment"
+          name="body"
+          value={post.body}
+          onChange={createPost}
+          required
+        />
+        <Button variant="contained" onClick={submit} style={{margin: '20px'}}>{edit ? "edit" : "submit"}</Button>
+      </FormControl>
+      {
+        loading && <Loader />
+      }
+      {posts.length!==0 && posts.map((post, index)=>
+        (<Accordion post={post} key={index} index={index} deletePost={deletePost} editPost={editPost} />
+      ))}
     </div>
   );
 }
